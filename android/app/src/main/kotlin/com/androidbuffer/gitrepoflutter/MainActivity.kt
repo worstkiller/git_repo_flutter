26ca31772c4dffa61035267a6f6211fddea4ee90
worstkilller <vkbookworm@gmail.com>
2018-10-11 16:00:13 +0530

    feat: added the native share for android and details ui

--- android/app/src/main/kotlin/com/androidbuffer/gitrepoflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/androidbuffer/gitrepoflutter/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.androidbuffer.gitrepoflutter
 
 import android.os.Bundle
-
+import android.content.Intent
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity(): FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity() : FlutterActivity() {
+
+    private val CHANNEL_SHARE = "git_repo_flutter/share_intent";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+        MethodChannel(flutterView, CHANNEL_SHARE).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "shareIntent" -> {
+                    shareIntent(call.argument("repo_url"))
+                }
+            }
+        }
+    }
+
+    private fun shareIntent(data: String) {
+        val sendIntent = Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, data);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
 }
--- lib/DetailsFragment.dart
+++ lib/DetailsFragment.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:git_repo_flutter/GitRepo.dart';
+import 'package:flutter/services.dart';
 
 class DetailsFragment extends StatelessWidget {
   final GitRepo gitRepo;
+  final platform = const MethodChannel("git_repo_flutter/share_intent");
 
   DetailsFragment({this.gitRepo});
 
@@ -12,16 +14,150 @@ class DetailsFragment extends StatelessWidget {
       appBar: AppBar(
         title: Text("Details"),
       ),
-      body: Center(
-        child: Text(gitRepo.name),
-      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Image.network(
+                gitRepo.owner.avatarUrl,
+                height: 80.0,
+                width: 80.0,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Text(
+                gitRepo.name,
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      right: 16.0,
+                    ),
+                    child: Icon(Icons.title),
+                  ),
+                  Flexible(
+                      child: Text(gitRepo.language != null
+                          ? "Language: ${gitRepo.language}"
+                          : "Language: Unknown"))
+                ],
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      right: 16.0,
+                    ),
+                    child: Icon(Icons.info),
+                  ),
+                  Flexible(child: Text("Description: ${gitRepo.description}"))
+                ],
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      right: 16.0,
+                    ),
+                    child: Icon(Icons.favorite),
+                  ),
+                  Flexible(child: Text("Forks: ${gitRepo.forksCount}"))
+                ],
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      right: 16.0,
+                    ),
+                    child: Icon(Icons.stars),
+                  ),
+                  Flexible(child: Text("Stars: ${gitRepo.stargazersCount}"))
+                ],
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      right: 16.0,
+                    ),
+                    child: Icon(Icons.bug_report),
+                  ),
+                  Flexible(child: Text("Issues: ${gitRepo.openIssuesCount}"))
+                ],
+              ),
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      right: 16.0,
+                    ),
+                    child: Icon(Icons.calendar_today),
+                  ),
+                  Flexible(child: Text("Updated at: ${gitRepo.updatedAt}"))
+                ],
+              ),
+            ),
+            Divider(),
+          ],
+        ),
+      )),
       floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () => handleFabClick(gitRepo),
           child: Icon(
             Icons.share,
             color: Colors.white,
             size: 20.0,
           )),
     );
+  }
+
+  handleFabClick(GitRepo gitRepo) async {
+    try {
+      platform.invokeMethod("shareIntent", {"repo_url": gitRepo.htmlUrl});
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
   }
 }
